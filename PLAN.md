7d67982bc5001bc9d4a4ecdf5e25388d69b9e217
Ryan <ryanshaunsmith@gmail.com>
2025-03-06 20:50:36 +0000

    update dice state to be an object with id and held state

--- PLAN.md
+++ PLAN.md
@@ -7,6 +7,9 @@
   - should take a value prop ✅
   - render ten on screen ✅
   - style ✅
-- create a random array of values
-  - store in state
-  - render array on screen
+- create a random array of values ✅
+  - store in state ✅
+  - render array on screen ✅
+- Turn dice state into an object ✅
+  - Include an id (for the key) ✅
+  - an isHeld boolean initially set to false ✅
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import "./App.css";
 import Die from "./components/Die/Die";
 
+interface diceObject {
+  id: string;
+  value: number;
+  isHeld: boolean;
+}
+
 function App() {
+  const [dice, setDice] = useState<diceObject[]>(() => generateNewRandomDice());
+
+  function generateNewRandomDice() {
+    return new Array(10).fill(0).map(() => ({
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    }));
+  }
+
+  const diceElements = dice.map((die) => {
+    return <Die key={die.id} value={die.value} />;
+  });
+
   return (
     <main className="game-container">
-      <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 }
